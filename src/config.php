1727d27845982631386e860ff8174e52cd5238ab
Sher Ali <bcsf15m012@pucit.edu.pk>
2018-05-30 01:07:37 +0500

    change db configuration

--- src/config.php
+++ src/config.php
@@ -1,8 +1,6 @@
 <?php
 
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "sherali");
-defined('DB_PWD')   ? null : define("DB_PWD", "1399");
-defined('DB_NAME')   ? null : define("DB_NAME", "sheralibaba");
-
-?>
+defined('DB_USER') ? null : define("DB_USER", "root");
+defined('DB_PASSWORD') ? null : define("DB_PASSWORD", "");
+defined('DB_NAME') ? null : define("DB_NAME", "sheralibaba");
