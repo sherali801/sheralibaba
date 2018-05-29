feef6d16ca6b536c1eef0567470c5b25e5a0e0bd
Sher Ali <bcsf15m012@pucit.edu.pk>
2018-05-30 01:26:55 +0500

    create template and separate in header and footer files

--- 404.php
+++ 404.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>404 - Error</title>
-</head>
-<body>
-    <h1>404 - Error</h1>
-</body>
-</html>
+<?php require_once "header.php"; ?>
+
+<h1>404 - Error</h1>
+
+<?php require_once "footer.php"; ?>
